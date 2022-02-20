d41da8fb3cc54e31ac2bd086d9d7ed488e983e43
Antonis Geralis <b3lievem@yandex.com>
2022-02-20 19:09:08 +0200

    add docs generation

--- nakefile.nim
+++ nakefile.nim
@@ -1,12 +1,14 @@
-import nake
+import nake, std/strformat
 # Only Linux is supported!
 
 const
-  RaylibStableCommit = "963de06d08f28784e03b6cfa994d545f9e0ef8b5"
-  SourceDir = currentSourcePath().parentDir
+  ProjectUrl = "https://github.com/planetis-m/raylib-fever".quoteShell
+  RaylibStableCommit = "d4382f4a52e7631bf02ff8073ed24b282596ce0a"
+  SourceDir = currentSourcePath().parentDir.quoteShell
   CIncludeDir = SourceDir / "cinclude"
   JsonApiDir = SourceDir / "api"
   RaylibDir = SourceDir / "dist" / "raylib"
+  DocsDir = SourceDir / "docs"
 
 proc fetchLatestRaylib =
   if not dirExists(RaylibDir):
@@ -43,3 +45,15 @@ task "wrap", "Produces the raylib nim wrapper":
     direShell("cc", parser.addFileExt(".c"), "-o", exe)
     direShell(exe, "-f JSON", "-d RLAPI", "-i", header, "-o", JsonApiDir / "raylib_api.json")
   generateWrapper()
+
+task "docs", "Generate documentation":
+  # https://nim-lang.github.io/Nim/docgen.html
+  let src = ["raylib.nim", "raymath.nim"]
+  for f in src.items:
+    echo &"Generating the docs for {f}..."
+    let doc = DocsDir / f.changeFileExt(".html")
+    if doc.needsRefresh(f):
+      direShell("nim doc",
+          &"--verbosity:0 --git.url:{ProjectUrl} --git.devel:main --git.commit:main --out:{DocsDir} {f}")
+    else:
+      echo &"Skipped generating {doc}."
