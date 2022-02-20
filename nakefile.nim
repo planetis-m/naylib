bab92949d3f53d2cb65e72526641c9ac2ec1309f
Antonis Geralis <b3lievem@yandex.com>
2022-02-20 22:03:35 +0200

    use builtin messages
    someday they may offer better text formatting

--- nakefile.nim
+++ nakefile.nim
@@ -3,7 +3,7 @@ import nake, std/strformat
 
 const
   ProjectUrl = "https://github.com/planetis-m/raylib-fever"
-  GitUrl = "https://github.com/raysan5/raylib.git"
+  RaylibGitUrl = "https://github.com/raysan5/raylib.git"
   RaylibStableCommit = "d4382f4a52e7631bf02ff8073ed24b282596ce0a"
   SourceDir = currentSourcePath().parentDir.quoteShell
   CIncludeDir = SourceDir / "cinclude"
@@ -13,37 +13,34 @@ const
 
 proc fetchLatestRaylib =
   if not dirExists(RaylibDir):
-    echo "Cloning ", GitUrl, "..."
-    direShell("git clone --depth 1", GitUrl, RaylibDir)
+    direSilentShell(&"Cloning {RaylibGitUrl}...",
+        "git clone --depth 1", RaylibGitUrl, RaylibDir)
   withDir(RaylibDir):
-    echo "Fetching latest stable commit..."
-    direShell("git fetch")
+    direSilentShell("Fetching latest stable commit...", "git fetch")
     direShell("git checkout", RaylibStableCommit)
 
 task "build", "Build the raylib C static library":
   fetchLatestRaylib()
   withDir(RaylibDir / "src"):
-    echo "Building raylib static library..."
     direShell("make clean")
-    direShell("make PLATFORM=PLATFORM_DESKTOP -j4")
+    direSilentShell("Building raylib static library...",
+        "make CC=clang PLATFORM=PLATFORM_DESKTOP -j4")
     # Install to cinclude directory
     discard existsOrCreateDir(CIncludeDir)
     copyFileToDir("libraylib.a", CIncludeDir)
     copyFileToDir("raylib.h", CIncludeDir)
-  echo "DONE!"
 
 proc generateWrapper =
   let script = "raylib_gen"
   withDir(SourceDir / "gen"):
     var exe = script.addFileExt(ExeExt)
     if exe.needsRefresh(script):
-      echo "Building ", script, "..."
-      direShell("nim c", "--mm:arc --panics:on -d:release -d:emiLenient", script)
+      direSilentShell(&"Building {script}...",
+          "nim c", "--mm:arc --panics:on -d:release -d:emiLenient", script)
     else:
       echo "Skipped building ", script
-    echo "Generating raylib Nim wrapper..."
     normalizeExe(exe)
-    direShell(exe)
+    direShell("Generating raylib Nim wrapper...", exe)
 
 task "wrap", "Produce the raylib nim wrapper":
   let parser = "raylib_parser"
@@ -52,25 +49,22 @@ task "wrap", "Produce the raylib nim wrapper":
   withDir(RaylibDir / "parser"):
     var exe = parser.addFileExt(ExeExt)
     if exe.needsRefresh(parser):
-      echo "Building ", parser, "..."
-      direShell("cc", parser.addFileExt(".c"), "-o", exe)
+      direSilentShell(&"Building {parser}...",
+          "cc", parser.addFileExt(".c"), "-o", exe)
     else:
       echo "Skipped building ", parser
-    echo "Generating API JSON file..."
     normalizeExe(exe)
-    direShell(exe, "-f JSON", "-d RLAPI", "-i", header, "-o", JsonApiDir / "raylib_api.json")
+    direSilentShell("Generating API JSON file...",
+        exe, "-f JSON", "-d RLAPI", "-i", header, "-o", JsonApiDir / "raylib_api.json")
   generateWrapper()
-  echo "DONE!"
 
 task "docs", "Generate documentation":
   # https://nim-lang.github.io/Nim/docgen.html
-  let src = ["raymath.nim", "raylib.nim"]
-  for f in src.items:
-    echo "Generating the docs for ", f, "..."
-    let doc = DocsDir / f.changeFileExt(".html")
-    if doc.needsRefresh(SourceDir / f):
-      direShell("nim doc",
-          &"--verbosity:0 --git.url:{ProjectUrl} --git.devel:main --git.commit:main --out:{DocsDir} {f}")
-    else:
-      echo "Skipped generating ", doc
-  echo "DONE!"
+  withDir(SourceDir):
+    for src in items(["raymath.nim", "raylib.nim"]):
+      let doc = DocsDir / src.changeFileExt(".html")
+      if doc.needsRefresh(src):
+        direSilentShell(&"Generating the docs for {src}...", "nim doc",
+            &"--verbosity:0 --git.url:{ProjectUrl} --git.devel:main --git.commit:main --out:{DocsDir}", src)
+      else:
+        echo "Skipped generating ", doc
