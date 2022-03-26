18bd3ced3925cf175e34682aeb1161bb0f6c0860
Antonis Geralis <b3lievem@yandex.com>
2022-03-26 22:09:38 +0200

    fix #13 add comparison operators for distincts

--- raylib.nim
+++ raylib.nim
@@ -354,6 +354,39 @@ const
   MaxMaterialMaps* = 12 ## Maximum number of shader maps supported
   MaxMeshVertexBuffers* = 7 ## Maximum vertex buffers (VBO) per mesh
 
+proc `==`*(a, b: ConfigFlags): bool {.borrow.}
+
+proc `<`*(a, b: TraceLogLevel): bool {.borrow.}
+proc `<=`*(a, b: TraceLogLevel): bool {.borrow.}
+proc `==`*(a, b: TraceLogLevel): bool {.borrow.}
+
+proc `==`*(a, b: KeyboardKey): bool {.borrow.}
+proc `==`*(a, b: MouseButton): bool {.borrow.}
+proc `==`*(a, b: MouseCursor): bool {.borrow.}
+proc `==`*(a, b: GamepadButton): bool {.borrow.}
+proc `==`*(a, b: GamepadAxis): bool {.borrow.}
+
+proc `<`*(a, b: MaterialMapIndex): bool {.borrow.}
+proc `<=`*(a, b: MaterialMapIndex): bool {.borrow.}
+proc `==`*(a, b: MaterialMapIndex): bool {.borrow.}
+
+proc `<`*(a, b: ShaderLocationIndex): bool {.borrow.}
+proc `<=`*(a, b: ShaderLocationIndex): bool {.borrow.}
+proc `==`*(a, b: ShaderLocationIndex): bool {.borrow.}
+
+proc `==`*(a, b: ShaderUniformDataType): bool {.borrow.}
+proc `==`*(a, b: ShaderAttributeDataType): bool {.borrow.}
+proc `==`*(a, b: PixelFormat): bool {.borrow.}
+proc `==`*(a, b: TextureFilter): bool {.borrow.}
+proc `==`*(a, b: TextureWrap): bool {.borrow.}
+proc `==`*(a, b: CubemapLayout): bool {.borrow.}
+proc `==`*(a, b: FontType): bool {.borrow.}
+proc `==`*(a, b: BlendMode): bool {.borrow.}
+proc `==`*(a, b: Gesture): bool {.borrow.}
+proc `==`*(a, b: CameraMode): bool {.borrow.}
+proc `==`*(a, b: CameraProjection): bool {.borrow.}
+proc `==`*(a, b: NPatchLayout): bool {.borrow.}
+
 type
   Enums = ConfigFlags|Gesture
   Flag*[E: Enums] = distinct uint32
