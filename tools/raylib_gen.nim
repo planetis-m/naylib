78146cae616d0f88a793b7f38e9b35dfd7dd90ba
Antonis Geralis <b3lievem@yandex.com>
2023-01-22 23:43:30 +0200

    remove linker flag

--- tools/raylib_gen.nim
+++ tools/raylib_gen.nim
@@ -39,7 +39,7 @@ elif defined(android):
 
   {.passL: "-Wl,-soname,lib" & ProjectLibraryName & ".so -Wl,--exclude-libs,libatomic.a".}
   {.passL: "-Wl,--build-id -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -Wl,--warn-shared-textrel".}
-  {.passL: "-Wl,--fatal-warnings -u ANativeActivity_onCreate -Wl,-undefined,dynamic_lookup".}
+  {.passL: "-Wl,--fatal-warnings -u ANativeActivity_onCreate -Wl,-no-undefined".}
   {.passL: "-llog -landroid -lEGL -lGLESv2 -lOpenSLES -lc -lm -ldl".}
 
 else:
