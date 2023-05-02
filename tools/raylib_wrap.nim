9a09159376dcefb246a789b22de9e2531c180ae8
Antonis Geralis <b3lievem@yandex.com>
2023-05-02 15:39:57 +0300

    add another one

--- tools/raylib_wrap.nim
+++ tools/raylib_wrap.nim
@@ -227,7 +227,7 @@ proc loadRenderTexture*(width: int32, height: int32): RenderTexture2D =
   result = loadRenderTexturePriv(width, height)
   if not isRenderTextureReady(result): raiseRaylibError("Failed to load RenderTexture")
 
-proc updateTexture*(texture: Texture2D, pixels: openArray[byte]; format = PixelFormat.UncompressedR8g8b8a8) =
+proc updateTexture*(texture: Texture2D, pixels: openArray[byte], format = PixelFormat.UncompressedR8g8b8a8) =
   ## Update GPU texture with new data, bytes overload
   assert texture.format == format, "Incompatible texture format"
   assert getPixelDataSize(texture.width, texture.height, texture.format) == pixels.len*sizeof(byte),
@@ -240,9 +240,17 @@ proc updateTexture*[T: Pixel](texture: Texture2D, pixels: openArray[T]) =
       "Mismatch between expected and actual data size"
   updateTexturePriv(texture, cast[pointer](pixels))
 
+proc updateTexture*(texture: Texture2D, rec: Rectangle, pixels: openArray[byte],
+    format = PixelFormat.UncompressedR8g8b8a8) =
+  ## Update GPU texture rectangle with new data
+  assert texture.format == format, "Incompatible texture format"
+  assert getPixelDataSize(rec.width.int32, rec.height.int32, texture.format) == pixels.len*sizeof(byte),
+      "Mismatch between expected and actual data size"
+  updateTexturePriv(texture, rec, cast[pointer](pixels))
+
 proc updateTexture*[T: Pixel](texture: Texture2D, rec: Rectangle, pixels: openArray[T]) =
   ## Update GPU texture rectangle with new data
-  assert getPixelDataSize(rec.width, rec.height, texture.format) == pixels.len*sizeof(T),
+  assert getPixelDataSize(rec.width.int32, rec.height.int32, texture.format) == pixels.len*sizeof(T),
       "Mismatch between expected and actual data size"
   updateTexturePriv(texture, rec, cast[pointer](pixels))
 
