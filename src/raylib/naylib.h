855e3cd7d3b0fc86368997a038e0e2e4a578e3e4
Antonis Geralis <b3lievem@yandex.com>
2024-10-24 23:02:46 +0300

    mistake

--- src/raylib/naylib.h
+++ src/raylib/naylib.h
@@ -1,11 +1,14 @@
-#include "raylib.h"
-
+// Windows API naming conflict workarounds
+#define Rectangle rlRectangle
 #define CloseWindow rlCloseWindow
 #define ShowCursor rlShowCursor
 #define LoadImage rlLoadImage
 #define DrawText rlDrawText
 #define DrawTextEx rlDrawTextEx
 
+#include "raylib.h"
+
+#undef Rectangle
 #undef CloseWindow
 #undef ShowCursor
 #undef LoadImage
