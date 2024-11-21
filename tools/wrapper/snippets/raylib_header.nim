6a3fdef1e63fa1d7945e5229fe757719e3867160
Antonis Geralis <b3lievem@yandex.com>
2024-11-21 13:13:07 +0200

    move config again

--- tools/wrapper/snippets/raylib_header.nim
+++ tools/wrapper/snippets/raylib_header.nim
@@ -2,7 +2,7 @@ from std/strutils import addf, toHex
 from std/unicode import Rune
 from std/syncio import writeFile
 import std/[assertions, paths]
-import rconfig
+import naylib/private/config
 const raylibDir = currentSourcePath().Path.parentDir / Path"raylib"
 
 {.passC: "-I" & raylibDir.string.}
